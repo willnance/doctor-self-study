35fa2bc3958e8bcbdea05e7c15558c12cd988858
Will Nance <wllnance@gmail.com>
2013-02-18 21:00:44 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DoctorSelfStudy::Application.config.secret_token = 'bdab9cbdb527de7a2019318d69565cee24c95ea9bd4d98fc2d741a4c42dec8b272e84c26d38a2c4e812646a9caff73a7a3f095f74d69a0656f30bedd1ad203f4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+DoctorSelfStudy::Application.config.session_store :cookie_store, key: '_doctor self study_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# DoctorSelfStudy::Application.config.session_store :active_record_store
